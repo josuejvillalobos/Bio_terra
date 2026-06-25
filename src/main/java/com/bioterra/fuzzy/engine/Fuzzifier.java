73f2bcd0207b626ef65f570a64e1d1900ecbb96c
SosueKurt78 <josue.villa778@gmail.com>
2026-06-25 15:45:57 -0600

    (feat): change classes to interfaces for Defuzzifier, Fuzzifier, and InterferenceEngine; implement MamdaniFuzzifier

--- src/main/java/com/bioterra/fuzzy/engine/Fuzzifier.java
+++ src/main/java/com/bioterra/fuzzy/engine/Fuzzifier.java
@@ -1,5 +1,12 @@
 package com.bioterra.fuzzy.engine;
 
-public class Fuzzifier {
-    
+import com.bioterra.fuzzy.model.LinguisticVariable;
+import java.util.Map;
+
+public interface Fuzzifier {
+
+    Map<String, Map<String, Double>> fuzzify(
+        Map<String, LinguisticVariable> variables,
+        Map<String, Double> crsipInputs
+    );
 }
